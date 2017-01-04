1a2a8ea70816fae9753a67f2c4a42fcc3b999df2
doowb <brian.woodward@gmail.com>
2017-01-04 15:56:58 -0500

    implementation

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 'use strict';
 
-module.exports = function() {
+var octicons = require('octicons');
+var extend = require('extend-shallow');
+
+module.exports = function(name, locals, options) {
+  if (!name || typeof name !== 'string') {
+    throw new Error('expected first argument to be a string');
+  }
+
+  if (typeof options !== 'object' && locals && locals.hash) {
+    options = locals;
+    locals = {};
+  }
+  locals = locals || {};
+  options = options || {};
+
+  // use `options.hash` when set from handlebars
+  var opts = extend({}, locals, options.hash);
+
+  if (!octicons[name]) {
+    if (opts.strict === false) {
+      return '';
+    }
+    throw new Error(`octicon "${name}" is not available`);
+  }
+  return octicons[name].toSVG(opts);
 };
